cde31a8fcba68f93d93769eb171001cf41e3d862
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-10-11 19:57:15 -0700

    module

---
+++ fibo.py
@@ -0,0 +1,11 @@
+def find_fibo(n):
+    if n <= 2:
+        return 1
+    fibo_x, fibo_next = 1,1
+
+    i = 3
+    while i <= n:
+        i = i + 1
+        fibo_x, fibo_next = fibo_next, fibo_x + fibo_next
+
+        return fibo_next
